334b5e1b0564384067d292f3ed38e53b4a9d037d
prakashgun <mail.prakash.india@gmail.com>
2021-08-25 23:34:09 +0530

    Fix CannotConnectAlreadyConnectedError warning

--- src/common/dbConnect.ts
+++ src/common/dbConnect.ts
@@ -1,23 +1,39 @@
-import { ConnectionOptions, createConnection, getConnection } from 'typeorm/browser'
+import { ConnectionOptions, createConnection, getConnection, getConnectionManager } from 'typeorm/browser'
 import { Account } from '../entities/Account'
 import { Category } from '../entities/Category'
 import { Transaction } from '../entities/Transaction'
 
+const options: ConnectionOptions = {
+    name: "default",
+    type: "react-native",
+    database: "track_exp",
+    location: "default",
+    logging: ["error", "query", "schema"],
+    synchronize: true,
+    entities: [Account, Category, Transaction],
+    extra: {
+        KeepConnectionAlive: true
+    }
+}
+
+const connectionManager = getConnectionManager()
+
 export default async () => {
-    const options: ConnectionOptions = {
-        name: "default",
-        type: "react-native",
-        database: "track_exp",
-        location: "default",
-        logging: ["error", "query", "schema"],
-        synchronize: true,
-        entities: [Account, Category, Transaction]
+    if (!connectionManager.has('default')) {
+        console.log('Connection not exists.')
+        connectionManager.create(options)
     }
 
     try {
-        await getConnection(options.name).close()
-        return createConnection(options)
+        const db = connectionManager.get()
+        console.log('Connecting db')
+        await db.connect()
     } catch (error) {
-        return createConnection(options)
+        if (error.name === "CannotConnectAlreadyConnectedError") {
+            console.log('Already db connected')
+        } else {
+            console.log('Error happened while connecting db')
+            console.log(error)
+        }
     }
 }
